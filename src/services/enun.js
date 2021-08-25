630001805081c0900631326cf5898f02dea9b172
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-08-25 05:37:17 -0300

    Ajust

--- src/services/enun.js
+++ src/services/enun.js
@@ -2,7 +2,7 @@ const status = [
     {label: 'Para fazer', value: 'To do'},
     {label: 'Em progresso', value: 'In progress'},
     {label: 'Bloqueada', value: 'Blocked'},
-    {label: 'Concluidas', value: 'Finished'},
+    {label: 'Concluida', value: 'Finished'},
 ]
 
 export {
